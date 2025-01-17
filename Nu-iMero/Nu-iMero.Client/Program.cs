50c906abf5875b9286c129fd1e03b570ee8db037
Aka Sebastian <holteasebastian@yahoo.com>
2025-01-17 18:54:20 +0200

    am adaugat serializarea binara xml, am instalat un nou pachet newtonsoft.json

--- Nu-iMero/Nu-iMero.Client/Program.cs
+++ Nu-iMero/Nu-iMero.Client/Program.cs
@@ -1,11 +1,16 @@
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Nu_iMero.Client;
+using Nu_iMero.Client.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
 builder.Services.AddAuthorizationCore();
 builder.Services.AddCascadingAuthenticationState();
 builder.Services.AddSingleton<AuthenticationStateProvider, PersistentAuthenticationStateProvider>();
+builder.Services.AddSingleton<SerializationService>();
+Console.WriteLine("SerializationService registered.");
+
+
 
 await builder.Build().RunAsync();
---
+++ Nu-iMero/Nu-iMero.Client/Services/SerializationService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+using Newtonsoft.Json;
+
+namespace Nu_iMero.Client.Services
+{
+    public class SerializationService
+    {
+        // JSON Serialization
+        public void SerializeToJson<T>(T obj, string filePath)
+        {
+            var json = JsonConvert.SerializeObject(obj, Formatting.Indented);
+            File.WriteAllText(filePath, json);
+        }
+
+        public T DeserializeFromJson<T>(string filePath)
+        {
+            if (!File.Exists(filePath))
+                throw new FileNotFoundException($"File {filePath} not found!");
+
+            var json = File.ReadAllText(filePath);
+            return JsonConvert.DeserializeObject<T>(json)!; // Deserialize JSON to object
+        }
+
+        // XML Serialization
+        public void SerializeToXml<T>(T obj, string filePath)
+        {
+            var serializer = new XmlSerializer(typeof(T));
+            using (var stream = new FileStream(filePath, FileMode.Create, FileAccess.Write))
+            {
+                serializer.Serialize(stream, obj);
+            }
+        }
+
+        public T DeserializeFromXml<T>(string filePath)
+        {
+            if (!File.Exists(filePath))
+                throw new FileNotFoundException($"File {filePath} not found!");
+
+            var serializer = new XmlSerializer(typeof(T));
+            using (var stream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+            {
+                return (T)serializer.Deserialize(stream)!; // Deserialize XML to object
+            }
+        }
+    }
+}
