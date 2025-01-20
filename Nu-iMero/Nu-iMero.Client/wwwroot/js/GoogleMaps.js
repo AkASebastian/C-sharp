b67c6f1a4d016e847cdd0be9622d5b1b2cfc78dd
Andrei-Antonio Saran <andrei-antonio.saran21@student.ub.ro>
2025-01-20 12:38:55 +0200

    am adaugat harta / map, cu API de la Google.

---
+++ Nu-iMero/Nu-iMero.Client/wwwroot/js/GoogleMaps.js
@@ -0,0 +1,17 @@
+﻿let map;
+
+window.initializeGoogleMap = (mapContainerId, latitude, longitude, zoom) => {
+    const mapOptions = {
+        center: { lat: latitude, lng: longitude },
+        zoom: zoom,
+    };
+    map = new google.maps.Map(document.getElementById(mapContainerId), mapOptions);
+};
+
+window.addGoogleMapMarker = (latitude, longitude, title) => {
+    const marker = new google.maps.Marker({
+        position: { lat: latitude, lng: longitude },
+        map: map,
+        title: title,
+    });
+};
--- Nu-iMero/Nu-iMero.Client/wwwroot/js/site.js
+++ Nu-iMero/Nu-iMero.Client/wwwroot/js/site.js
@@ -5,3 +5,31 @@ function downloadFile(filename, base64Data) {
     link.download = filename;
     link.click();
 }
+
+function initializeGoogleMap(apiKey) {
+    const script = document.createElement('script');
+    script.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&callback=initMap&libraries=maps,marker&v=beta`;
+    script.async = true;
+    script.defer = true;
+    document.head.appendChild(script);
+}
+
+function initMap() {
+    const mapElement = document.getElementById('map');
+    if (!mapElement) {
+        console.error('Map element not found');
+        return;
+    }
+
+    const map = new google.maps.Map(mapElement, {
+        center: { lat: 40.12150192260742, lng: -100.45039367675781 },
+        zoom: 4,
+        mapId: 'DEMO_MAP_ID',
+    });
+
+    new google.maps.marker.AdvancedMarkerView({
+        map: map,
+        position: { lat: 40.12150192260742, lng: -100.45039367675781 },
+        title: "My location",
+    });
+}
